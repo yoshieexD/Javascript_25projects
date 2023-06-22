9dddc40fa51b6f3bc159d9a1c7bb03cc01a93d70
Jom-Winston <bjomwinston0107@gmail.com>
2023-06-23 01:46:13 +0800

    done

--- form_validation/app.js
+++ form_validation/app.js
@@ -23,7 +23,30 @@ function checkInputs(){
         setSuccessFor(username);
 
     }
+    if(emailValue === ''){
+        setErrorFor(email, 'Email cannot be blank');
+    }
+    else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Not a valid email');
+}   else {
+    setSuccessFor(email);
 }
+    if(passwordValue === '') {
+    setErrorFor(password, 'Password cannot be blank');
+}    else {
+    setSuccessFor(password);
+}
+
+    if(password2Value === '') {
+    setErrorFor(password2, 'Password2 cannot be blank');
+}   else if(passwordValue !== password2Value) {
+    setErrorFor(password2, 'Passwords does not match');
+}  else{
+    setSuccessFor(password2);
+}
+}
+
+
 
 function setErrorFor(input,message){
     const formControl = input.parentElement;
@@ -37,5 +60,8 @@ function setErrorFor(input,message){
 function setSuccessFor(input){
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
+}
 
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
