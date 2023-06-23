76ada3b41dbbbac1c47da440731be31db5fa20d4
Jom-Winston <bjomwinston0107@gmail.com>
2023-06-23 23:41:45 +0800

    done

--- stepprogressbar/app.js
+++ stepprogressbar/app.js
@@ -1,11 +1,23 @@
 const circles = document.querySelectorAll('.circle');
-const progressbar = document.querySelectorAll('.indicator');
+const progressbar = document.querySelector('.indicator');
 const buttons = document.querySelectorAll('button');
 
 let currentStep = 1;
 const updateSteps =(e) =>{
 currentStep = e.target.id === "next" ? ++currentStep: --currentStep;
-console.log(currentStep);
+
+circles.forEach((circle, index) => {
+    circle.classList [`${index < currentStep ? "add" : "remove"}`]("active");
+});
+
+progressbar.style.width = `${((currentStep -1)/ (circles.length -1)) * 100}%`
+if(currentStep ===circles.length){
+    buttons[1].disabled = true;
+} else if (currentStep === 1 ){
+    buttons[0].disabled = true;
+} else{
+    buttons.forEach((button) => (button.disabled = false));
+}
 }
 buttons.forEach(button => {
     button.addEventListener("click", updateSteps);
